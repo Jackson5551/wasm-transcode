0411c6e54bc3c0939e7858ee4bc1f39f69557650
Jackson Bingham <bingham.jackson49@gmail.com>
2025-07-07 15:24:11 -0600

    Started working on the backend workers

---
+++ worker/src/index.ts
@@ -0,0 +1,19 @@
+
+// For AutoRouter documentation refer to https://itty.dev/itty-router/routers/autorouter
+import { AutoRouter } from 'itty-router';
+
+let router = AutoRouter();
+
+// Route ordering matters, the first route that matches will be used
+// Any route that does not return will be treated as a middleware
+// Any unmatched route will return a 404
+router
+  .get('/', () => new Response('Hello, Spin!'))
+  .get('/hello/:name', ({ name }) => `Hello, ${name}!`)
+  .get('/uploads/:id', ({id}) => `File: ${id}`)
+
+//@ts-ignore
+addEventListener('fetch', (event: FetchEvent) => {
+  event.respondWith(router.fetch(event.request));
+});
+
